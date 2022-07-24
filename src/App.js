0764e4ffc7138a59876d0ff17c0876ab6a613bb7
Brito_Jaison <britojaison1123@gamil.com>
2022-07-24 14:27:39 +0530

    this commit is after 3 days of work. created entery form,bmi score calculaton and srceen.now created bmi list, only list. have to calculation difference in weight to normal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { useState } from 'react';
+import BmiList from './components/BmiList';
+import Bmiscore from './components/Bmiscore';
+import Form from './components/Form'
+
+function App() 
+{
+  const [bmi, setbmi] = useState("")
+  const [bmiType, setbmiType] = useState("nice")
+  const onFormSub = (w, h) => {
+    let b = calbmi(w, h);
+    setbmi(b);
+    let type = weightType(b);
+    console.log(b)
+    
+    setbmiType(type);
+  }
+  const calbmi = (w, h) => {
+    return (w / (h * h)).toFixed(2);
+  };
+  const weightType = (bmi) => {
+    if (bmi < 18.5) {
+      return "Underweight";
+    } else if (18.5 < bmi && bmi < 24.9) {
+      return "Normal";
+    } else if (24.9 <bmi && bmi < 29.9) {
+      return "Over Weight";
+    } else if (29.9 < bmi && bmi < 34.9) {
+      return "Obesity Class I";
+    } else if (34.9 < bmi && bmi < 39.9) {
+      return "Obesity Class II";
+    } else if (bmi > 39.9){
+      return "Obesity Class III";
+    }
+  };
+
+
+return (
+  <>
+    <div className='container'>
+      <div className='row justify-content-center mt-5 mx-2'>
+        <Form getData={onFormSub} />
+      </div>
+      <div className='row justify-content-center mt-5 mx-2'>
+        <div className='col-12 col-sm-6 mb-5'>
+          <Bmiscore bmiNo={bmi} bmiName={bmiType} />
+        </div>
+        <div className='col-12 col-sm-6'>
+          <BmiList />
+        </div>
+      </div>
+
+
     </div>
-  );
+  </>
+ );
 }
 
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+
+function Form({getData}) {
+  const [weight, setweight] = useState("");
+  const [height, setheight] = useState("");
+  const [alert, setalert] = useState(false);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (isNaN(weight) || isNaN(height)) {
+      setalert(true);
+      console.log("input is not valid");
+    }
+    else {
+      getData(weight,height)
+      setalert(false);
+    
+    }
+
+  };
+  let alertMessage
+  if (alert) {
+    alertMessage = <div className="alert alert-warning" role="alert">data you given are incorrect</div>
+  }
+  else {
+    alertMessage = ""
+  }
+  return (
+
+    <div className="col-sm-4 shadow rounded px-5">
+      <h1 className="text-center pt-3 text-secondary h2">BMI Calculator</h1>
+      <form onSubmit={onSubmit}>
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="my-3">
+              <label >Weight</label>
+              <input type="text"
+                value={weight}
+                onChange={(e) => setweight(e.target.value)} className="form-control"
+                required
+              />
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="my-3">
+              <label >Heigth</label>
+              <input type="text"
+                value={height}
+                onChange={(e) => setheight(e.target.value)} className="form-control"
+                required
+              />
+            </div>
+          </div>
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Get BMI" />
+
+      </form>
+      {alertMessage}
+    </div>
+
+  )
+}
+
+export default Form
